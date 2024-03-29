0e323621238b396de52546dc5f20ef10bc0429d4
footility <paolo.mistretta@footility.com>
2024-03-29 16:14:46 +0100

    try 1 to fix namespace

--- src/Http/FooCostServiceProvider.php
+++ src/Http/FooCostServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Footility\FooCost;
+namespace Footility\FooCost\Http;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -8,17 +8,17 @@ class FooCostServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../views', 'foocost');
         $this->publishes([
-            __DIR__.'/../config/foocost.php' => config_path('foocost.php'),
+            __DIR__ . '/../config/foocost.php' => config_path('foocost.php'),
         ]);
     }
 
     public function register()
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/foocost.php', 'foocost'
+            __DIR__ . '/../config/foocost.php', 'foocost'
         );
     }
 }
